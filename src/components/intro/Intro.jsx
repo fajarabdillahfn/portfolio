57edc454a4a0511c71c715af12e7b49323a463f2
Abdillah Fajar <abdillah.fajar@tokopedia.com>
2022-01-19 17:29:08 +0700

    add intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,39 @@
-import './intro.scss'
+import './intro.scss';
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
-    return (
-        <div className='intro'>
-            
+  const textRef = useRef();
+
+  useEffect(()=>{
+    init(textRef.current, {
+        showCursor: true,
+        cursorChar: "_",
+        backDelay: 1500,
+        backSpeed: 150,
+        strings: ["Developer", "Tester", "Engineer"]
+    })
+  },[])
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
         </div>
-    )
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Abdillah Faisal Nur Fajar</h1>
+          <h3>
+            Software <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,32 @@
-import './topbar.scss'
+import './topbar.scss';
+import PersonIcon from '@mui/icons-material/Person';
+import MailIcon from '@mui/icons-material/Mail';
 
-export default function Topbar() {
-    return (
-        <div className='topbar'>
-            
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={'topbar ' + (menuOpen && 'active')}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            aurinko.
+          </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>+62 822 933 4121</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>fajar.abdillahfn@gmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
