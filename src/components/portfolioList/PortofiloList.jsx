f2e9b0f96a1f2134ae7b10bcb701e94da40d1e19
Abdillah Fajar <abdillah.fajar@tokopedia.com>
2022-01-20 08:57:57 +0700

    portfolio page and data

---
+++ src/components/portfolioList/PortofiloList.jsx
@@ -0,0 +1,11 @@
+import './portfolioList.scss';
+
+export default function PortofiloList({ title, active, setSelected, id }) {
+  return (
+    <li
+      className={active ? 'portfolioList active' : 'portfolioList'}
+      onClick={() => setSelected(id)}>
+      {title}
+    </li>
+  );
+}
